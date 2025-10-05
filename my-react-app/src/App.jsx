aef135291727c9a4ce5ddf9fe81d59e77d7a2e96
Yerko Navarro <yerkonavarro7@gmail.com>
2025-10-05 02:20:44 -0300

    implementa_navbar

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import TypesExample from './components/boton'
+
+
+
+import NavBar from './components/nav_bar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <TypesExample />
+      <NavBar />
+
+       <section class="hero-banner text-center text-white d-flex flex-column justify-content-center align-items-center" role="banner" aria-label="Banner principal">
+      <div class="big-title">
+        <h1 class="display-4">¡50 Años Endulzando Chile!</h1>
+        <p class="lead">Participamos en el récord Guinness de la torta más grande del mundo 🎂</p>
+        <a href="#" class="button-explorar-productos" role="button">Explorar Productos</a>
+      </div>
+    </section>
     </>
   )
 }
