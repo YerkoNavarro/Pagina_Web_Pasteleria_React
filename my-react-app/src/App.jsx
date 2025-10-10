f9c14f77f1aba3586b366bc26f36c1d4fa599d6b
Yerko Navarro <yerkonavarro7@gmail.com>
2025-10-10 15:45:07 -0300

    routes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,34 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
-
-
-
-import NavBar from './components/nav_bar'
-import UnFooter from './components/C_footer'
-import UnCarrusel from './components/carrusel'
-
+import Inicio from './paginas/inicio'
 
 function App() {
-
-
   return (
-    <>
-      <NavBar />
-
-      <section class="hero-banner text-center text-white d-flex flex-column justify-content-center align-items-center" role="banner" aria-label="Banner principal">
-        <div class="big-title">
-          <h1 class="display-4">¡50 Años Endulzando Chile!</h1>
-          <p class="lead">Participamos en el récord Guinness de la torta más grande del mundo 🎂</p>
-          <a href="#" class="button-explorar-productos" role="button">Explorar Productos</a>
-        </div>
-      </section>
-
-      <UnCarrusel />
-
-      <UnFooter />
-    </>
+    <Routes>
+      <Route path="/" element={<Inicio />} />
+    </Routes>
   )
 }
 
