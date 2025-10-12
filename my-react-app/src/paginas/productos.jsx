f0cb05e6b205ce2a4c9763ca3ea4d8a41ff36d16
YerkoNavarro <yerkonavarro7@gmail.com>
2025-10-11 21:10:44 -0300

    implementa card y  list

--- my-react-app/src/paginas/productos.jsx
+++ my-react-app/src/paginas/productos.jsx
@@ -1,9 +1,53 @@
 import NavBar from '../components/nav_bar'
 
 import UnFooter from '../components/C_footer'
-
+import ProductCard from '../components/card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import imagenAlfajor from '../imagenes/Gemini_Generated_Image_alfajor.png'
+import imagenCupcakes from '../imagenes/Gemini_Generated_Image_i37ja2i37ja2i37j.png'
+import imagenPastel from '../imagenes/Gemini_Generated_Image_pastel.png'
+import imagenPieLimon from '../imagenes/Gemini_Generated_Image_pie_limon.png'
+import imagenBrownie from '../imagenes/Gemini_Generated_Image_z5gheyz5gheyz5gh.png'
 
 function Productos() {
+    const productos = [
+        {
+            Nombre: 'Pastel de Chocolate',
+            Descripcion: 'Pastel de chocolate con un toque de vainilla',
+            Precio: '1000',
+            Imagen: imagenPastel
+        },
+        
+        {
+            Nombre: 'Pie de Limón',
+            Descripcion: 'Relleno cítrico con merengue italiano tostado',
+            Precio: '900',
+            Imagen: imagenPieLimon
+        },
+        
+        {
+            Nombre: 'Brownies de Nuez',
+            Descripcion: 'Brownies húmedos con trozos de nuez tostada',
+            Precio: '800',
+            Imagen: imagenBrownie
+        },
+        {
+            Nombre: 'Alfajor Artesanal',
+            Descripcion: 'Doble capa de galleta y dulce de leche cubierto en chocolate',
+            Precio: '700',
+            Imagen: imagenAlfajor
+        },
+        
+        {
+            Nombre: 'Cupcakes Decorados',
+            Descripcion: 'Caja de seis cupcakes con buttercream de colores',
+            Precio: '950',
+            Imagen: imagenCupcakes
+        },
+        
+    ]
+
     return (
         <>
             <NavBar />
@@ -11,7 +55,20 @@ function Productos() {
                 <div className="big-title">
                 <h1 className="display-4">Productos</h1>
                 </div>
+            <div className="my-5" /> 
             </section>
+            <Row className="g-4 justify-content-center">
+            {productos.map(producto => (
+                <Col key={producto.Nombre} xs={12} sm={6} md={4} lg={3}>
+                    <ProductCard
+                        Nombre={producto.Nombre}
+                        Descripcion={producto.Descripcion}
+                        Precio={producto.Precio}
+                        Imagen={producto.Imagen}/>
+                </Col>
+            ))}
+            </Row>
+            <div className="my-5" /> {/* espaciado */}
             <UnFooter />
         </>
     )
