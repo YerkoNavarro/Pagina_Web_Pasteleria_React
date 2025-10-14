c7f5d157032c673a7e6a9a8d93932d7bf149aab9
Yerko Navarro <yerkonavarro7@gmail.com>
2025-10-14 16:45:20 -0300

    arreglo carrusel

--- my-react-app/src/components/carrusel.jsx
+++ my-react-app/src/components/carrusel.jsx
@@ -1,40 +1,37 @@
 import Carousel from 'react-bootstrap/Carousel';
-import pastelImg from '../imagenes/images.jpeg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function UnCarrusel() {
+function UnCarrusel({ imagen1, imagen2, imagen3, descripcion1, descripcion2, descripcion3 }) {
   return (
-    <Carousel>
+    <Carousel style={{ maxWidth: '800px', margin: '0 auto' }}>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={pastelImg}
-          alt="Imagen pastel - First slide"
+          src={imagen1}
+          alt="Primera imagen del carrusel"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>{descripcion1}</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={pastelImg}
-          alt="Imagen pastel - Second slide"
+          src={imagen2}
+          alt="Segunda imagen del carrusel"
         />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>{descripcion2}</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={pastelImg}
-          alt="Imagen pastel - Third slide"
+          src={imagen3}
+          alt="Tercera imagen del carrusel"
         />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          <h3>{descripcion3}</h3>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
