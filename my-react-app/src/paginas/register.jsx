04f7a5f37dc521717945ff50d5c8d416bf922df6
Yerko Navarro <yerkonavarro7@gmail.com>
2025-10-15 23:48:26 -0300

    agrega paginas

---
+++ my-react-app/src/paginas/register.jsx
@@ -0,0 +1,19 @@
+
+import NavBar from '../components/nav_bar'
+import UnFooter from '../components/C_footer'
+function Register(){
+    return(
+        <>
+        <NavBar/>
+        <section className="hero-banner text-center text-white d-flex flex-column justify-content-center align-items-center" role="banner" aria-label="Banner principal">
+            <div className="big-title">
+            <h1 className="display-4">Registrarse</h1>
+            </div>
+        </section>
+        <div className="my-5" /> 
+        <UnFooter/>
+        </>
+    );
+}
+
+export default Register;
