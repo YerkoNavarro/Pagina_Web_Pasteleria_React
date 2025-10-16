3285dac9c097e3883474849b4c2799b6b1447779
Yerko Navarro <yerkonavarro7@gmail.com>
2025-10-15 23:47:58 -0300

    crea contacto.jsx

---
+++ my-react-app/src/paginas/contacto.jsx
@@ -0,0 +1,24 @@
+
+import NavBar from '../components/nav_bar';
+import UnFooter from '../components/C_footer';
+
+function Contacto() {
+    return (
+        <>
+            <NavBar />
+            <section 
+                className="hero-banner text-center text-white d-flex flex-column justify-content-center align-items-center" 
+                role="banner" 
+                aria-label="Banner principal"
+            >
+                <div className="big-title">
+                    <h1 className="display-4">Contacto</h1>
+                </div>
+            </section>
+            <div className="my-5" />
+            <UnFooter />
+        </>
+    );
+}
+
+export default Contacto;
