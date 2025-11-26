d1d2ece4d6b44d8b5b45e28b7bd8b0b22c8e9372
YerkoNavarro <yerkonavarro7@gmail.com>
2025-11-26 20:02:58 -0300

    "configuracion api"

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  server:{
+    port:5173,
+    proxy:{
+      '/api':{
+        target:process.env.API_BASE_URL,
+        changeOrigin:true,
+        
+      }
+    }
+  }
 })
